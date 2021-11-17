c5131f2c11acf587484fb4b7d6be813b6053e434
AnatoliyPim <92373418+AnatoliyPim@users.noreply.github.com>
2021-11-17 16:36:36 +0200

    chapter_1

---
+++ chapter_1/test_count_syllables_words_full_corpus.py
@@ -0,0 +1,17 @@
+import sys
+import count_syllables
+
+with open('train.txt') as in_file:
+    words = set(in_file.read().split())
+
+missing = []
+
+for word in words:
+    try:
+        num_syllables = count_syllables.count_syllables(word)
+        ##print(word, num_syllables, end='\n')  # раскоментируйте для показа количеств слов
+    except KeyError:
+        missing.append(word)
+print("Отсутсвующие слова:", missing, file=sys.stderr)
+
+
